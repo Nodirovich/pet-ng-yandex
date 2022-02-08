bd7cb4d064f13465f3959bd1b649b6c6c44d6d36
Nodirovich <uygunhujaev.aziz@payme.uz>
2022-02-08 17:36:27 +0500

    add: store

---
+++ libs/shared/store/src/index.ts
@@ -0,0 +1,6 @@
+export * from './lib/home/home.facade';
+export * from './lib/home/home.models';
+export * from './lib/home/home.selectors';
+export * from './lib/home/home.reducer';
+export * from './lib/home/home.actions';
+export * from './lib/shared-store.module';
---
+++ libs/shared/store/src/lib/home/home.actions.ts
@@ -0,0 +1,18 @@
+import { createAction, props } from '@ngrx/store';
+import { HomeEntity } from './home.models';
+
+export interface IHomeState {
+  play: boolean;
+}
+
+export const init = createAction('[Home Page] Init');
+
+export const loadHomeSuccess = createAction(
+  '[Home/API] Load Home Success',
+  props<{ home: HomeEntity[] }>()
+);
+
+export const loadHomeFailure = createAction(
+  '[Home/API] Load Home Failure',
+  props<{ error: any }>()
+);
